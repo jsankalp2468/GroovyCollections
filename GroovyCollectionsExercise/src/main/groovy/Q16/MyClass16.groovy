a43caa74b8e8056e9796cbfa54fa9591b99c56e8
jsankalp2468 <sankalp.jain@tothenew.com>
2018-03-26 16:09:36 +0530

    Q16 Completed

---
+++ GroovyCollectionsExercise/src/main/groovy/Q16/MyClass16.groovy
@@ -0,0 +1,13 @@
+package Q16
+
+class MyClass16 {
+    static void main(String[] args) {
+        Map<String,Integer> friend = ["sankalp":20, "shivam":21,"uditya":22, "suraj":24, "neelesh":25,
+                                      "sooraj":23,"satyam":25, "ankit":25, "ujjawl":24, "swapnil":22]
+
+        println("Map : "+friend)
+    }
+}
+
+
+//Create a new map consisting of 10 of your friend's name's as keys and their ages as value.
