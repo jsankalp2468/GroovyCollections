7ad1b6ccb7988e4601309d3fa0ffb4118db820bf
jsankalp2468 <sankalp.jain@tothenew.com>
2018-03-26 15:29:18 +0530

    Q10 Completed

---
+++ GroovyCollectionsExercise/src/main/groovy/Q10/MyClass10.groovy
@@ -0,0 +1,11 @@
+package Q10
+
+class MyClass10 {
+    public static void main(String[] args) {
+        println((1..20).first())
+        println((1..20).last())
+        println((1..20).subList(1,2).last())
+    }
+}
+
+//Get first, second and last element of Range.
