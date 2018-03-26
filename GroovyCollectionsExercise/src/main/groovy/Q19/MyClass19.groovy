776f7f09d8b27b73a79dd948b26823d82ca6dfc5
jsankalp2468 <sankalp.jain@tothenew.com>
2018-03-26 16:20:43 +0530

    Q19 Completed

---
+++ GroovyCollectionsExercise/src/main/groovy/Q19/MyClass19.groovy
@@ -0,0 +1,14 @@
+package Q19
+
+class MyClass19 {
+    public static void main(String[] args) {
+        Map m = ['1' : 2, '2' : 3, '3' : 4, '2':5]
+        println("Map : "+m)
+        println("m['2'] : "+m['2'])
+    }
+}
+
+//Consider the following map:
+// Map m = ['1' : 2, '2' : 3, '3' : 4, '2':5]
+// Is this a valid construction?
+// What is the value of m['2']?
