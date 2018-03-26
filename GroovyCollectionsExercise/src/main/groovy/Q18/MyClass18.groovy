07088906e3946078b40f2ca66f59ea47ba213f03
jsankalp2468 <sankalp.jain@tothenew.com>
2018-03-26 16:16:20 +0530

    Q18 Completed

---
+++ GroovyCollectionsExercise/src/main/groovy/Q18/MyClass18.groovy
@@ -0,0 +1,12 @@
+package Q18
+
+class MyClass18 {
+    public static void main(String[] args) {
+        Map<Integer,String> map1 = [1:"san",2:"wan1"]
+        println("map.class : "+map1.class)
+        println("map.getClass() : "+map1.getClass())
+    }
+}
+
+//Try the following code on a map: println map.class println map.getClass()
+// What do you observe?
