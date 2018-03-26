a00232c0d22d07345810776a969e2274f0b0f7e5
jsankalp2468 <sankalp.jain@tothenew.com>
2018-03-26 16:24:06 +0530

    Q20 Completed

---
+++ GroovyCollectionsExercise/src/main/groovy/Q20/MyClass20.groovy
@@ -0,0 +1,10 @@
+package Q20
+
+class MyClass20 {
+    public static void main(String[] args) {
+        Map m = ['1' : 2, '2' : 3, '3' : 4, '2':5]
+        println("m.containsKey('2') : "+m.containsKey('2'))
+    }
+}
+
+//Find if a map contains a particular key.
