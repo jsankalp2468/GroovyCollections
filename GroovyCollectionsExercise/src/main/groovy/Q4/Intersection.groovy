ffe18405f009cd273d6520930c6ead88f0c6bf9e
jsankalp2468 <sankalp.jain@tothenew.com>
2018-03-26 15:23:22 +0530

    Q4 Completed

---
+++ GroovyCollectionsExercise/src/main/groovy/Q4/Intersection.groovy
@@ -0,0 +1,13 @@
+package Q4
+
+class Intersection {
+    static void main(String[] args) {
+        List list = [11,12,13,14]
+        List list1 = [13,14,15]
+        println("Common elements in a list using list.intersect(list1) : "
+                +list.intersect(list1))
+    }
+}
+
+
+//Find whether two lists have a common element or not
