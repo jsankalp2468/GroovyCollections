b871ef82c4e4b3a395345578258a67d15d0c1ab3
jsankalp2468 <sankalp.jain@tothenew.com>
2018-03-26 15:22:07 +0530

    Q3 Completed

---
+++ GroovyCollectionsExercise/src/main/groovy/Q3/MyClass3.groovy
@@ -0,0 +1,13 @@
+package Q3
+
+class MyClass3 {
+    static void main(String[] args) {
+        List list = [11,12,13,14]
+        List list1 = [13,14,15]
+        println("list-list1 : "+(list-list1))
+//        println(list1.intersect(list))
+    }
+}
+
+//Given two lists [11, 12, 13, 14] and [13, 14, 15],
+// how would we obtain the list of items from the first that are not in the second?
